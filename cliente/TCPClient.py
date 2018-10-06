3871885699fc58227b28115691f6e4d933210e09
Juan Camilo Usche <jc.useche10@uniandes.edu.co>
2018-10-06 09:41:00 -0500

    ready to deploy

--- cliente/TCPClient.py
+++ cliente/TCPClient.py
@@ -1,12 +1,13 @@
-import socket, sys, threading, hashlib,time, logging,os
+import socket, sys, hashlib
 
-
+#USAR ESTO PARA ALTERNAR LAS PRUEBAS ENTRE LOCAL Y REMOTO
 host, port = '157.253.205.19', 9000
 #host, port = 'localhost', 9000
 hasher = hashlib.md5()
 SIZE=2048
 
 class recv_data :
+
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     clientsocket.connect((host, port))
@@ -15,21 +16,19 @@ class recv_data :
         data = self.clientsocket.recv(SIZE)
         i=0
         bytesReceived=0
-        with open(sys.argv[1], 'wb+') as f:
+        #EL CLIENTE EMPEZARA A ESCRIBIR LO QUE RECIBE EN UN destiny_file
+        with open(sys.argv[1], 'wb+') as destiny_file:
             while data != bytes(''.encode()):
-                f.write(data)
+                destiny_file.write(data)
                 data = self.clientsocket.recv(SIZE)
                 bytesReceived=bytesReceived+len(data);
                 i=i+1
                 if data == b'Fin' :
-                    print('Fin de archivo')
                     break
-
-            buf = f.read()
+            buf = destiny_file.read()
             hasher.update(buf)
             hash_cliente = hasher.hexdigest()
-            print('hash: ', hash_cliente)
-            print('termino')
+            print('DONE')
 
 
 
--- servidor/TCPServer.py
+++ servidor/TCPServer.py
@@ -1,17 +1,16 @@
 
 import socket, sys, threading, hashlib,time, logging,os
-from time import gmtime, strftime
-from time import sleep
 from sys import stderr
+from time import gmtime, strftime
 from logging import getLogger, StreamHandler, Formatter, DEBUG
-
+#ESTAS SIMPLEMENTE SON VARIABLES
 host, port = '', 9000
-TAMANO = 2048
 hasher = hashlib.md5()
+TAMANO = 2048
 tamano = 0
 
 class transfer :
-
+    #ESTE ES EL SOCKET QUE VAMOS  USAR
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -19,14 +18,16 @@ class transfer :
         numCli = int(sys.argv[1])
         file_name = sys.argv[2]
         self.serversocket.bind((host, port))
-        print(' Server ready for connections')
+
+        print('Ready to receive connections')
         self.serversocket.listen(5)
+        id_cliente = 1
         threads = []
-        id_cliente =1
-        while numCli >0:
+
+        #VAMOS A ESPERAR A QUE SE CONECTEN TODOS LOS CLIENTES ANTES DE INICIAR LOS THREADS QUE TRANSFIEREN EL ARCHIVO A CADA UNO
+        while numCli > 0 :
             tamano = os.path.getsize(file_name)
             conn, addr = self.serversocket.accept()
-            print(' file TAMANO : {}'.format(str(TAMANO)))
             send_thread = threading.Thread(target = self.send_file, args=(file_name, tamano, conn, id_cliente ))
             threads.append(send_thread)
             numCli = numCli - 1
@@ -34,22 +35,23 @@ class transfer :
         for thread in threads:
             thread.start()
 
+    #AQUI INICIA    N LOS THREADS
     def send_file(self, file_name, tamano, conn, id_cliente):
 
-        l = getLogger()
+        logger = getLogger()
         os.makedirs(os.path.dirname('./logs/TCP{}.log'.format(id_cliente)), exist_ok=True)
         logging.basicConfig(format='%(message)s', filename='./logs/TCP{}.log'.format(id_cliente), level=logging.DEBUG)
         sh = StreamHandler(stderr)
         sh.setLevel(DEBUG)
         f = Formatter(' %(message)s')
         sh.setFormatter(f)
-        l.addHandler(sh)
-        l.setLevel(DEBUG)
+        logger.addHandler(sh)
+        logger.setLevel(DEBUG)
         showtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        l.info('%s#%s', 'FECHA', showtime)
-        l.info('%s#%s', 'NOMBRE_ARCHIVO', file_name)
-        l.info('%s#%s', 'TAMANO_ARCHIVO', tamano)
-        l.info('%s#%s', 'ID_CLIENTE', id_cliente)
+        logger.info('%s#%s', 'FECHA', showtime)
+        logger.info('%s#%s', 'NOMBRE_ARCHIVO', file_name)
+        logger.info('%s#%s', 'TAMANO_ARCHIVO', tamano)
+        logger.info('%s#%s', 'ID_CLIENTE', id_cliente)
         i = 0
         bytesSent = 0
         with open(file_name, 'rb') as file:
@@ -69,17 +71,12 @@ class transfer :
                     break
 
             elapsed_time = time.time() - start_time
-            print(' File sent successfully.')
-            l.info('FILE_DELIVERY;SUCCESS')
-            l.info('%s#%s', 'BYTES_ENVIADOS', bytesSent)
-            l.info('%s#%s', 'BYTES_RECIBIDOS', bytesSent)
-            l.info('%s#%s', 'PAQUETES_ENVIADOS', i)
-            l.info('%s#%s', 'PAQUETES_RECIBIDOS', i)
-            l.info('%s#%s', 'TIEMPO_TOTAL', elapsed_time)
-            l.info('-----------------------------------')
-
-
-
-
+            logger.info('ENVIO_DEL_ARCHIVO#EXITOSO')
+            logger.info('%s#%s', 'BYTES_ENVIADOS', bytesSent)
+            logger.info('%s#%s', 'BYTES_RECIBIDOS', bytesSent)
+            logger.info('%s#%s', 'PAQUETES_ENVIADOS', i)
+            logger.info('%s#%s', 'PAQUETES_RECIBIDOS', i)
+            logger.info('%s#%s', 'TIEMPO_TOTAL', elapsed_time)
+            logger.info('----------------------------')
 
 Transfer = transfer()
